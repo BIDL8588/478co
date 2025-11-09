b55b9269653a86b513c5a88413d68d53ca392219
BIDL8588 <lopezbyron8588@gmail.com>
2025-11-08 20:05:33 -0800

    Update client.c

--- client.c
+++ client.c
@@ -8,70 +8,80 @@
 #define PORT 31337
 #define BUF_SIZE 1024
 
-int conn_serv(void);
-void message_ex(int m_fd);
-int main(void) {
-    int fd = conn_serv();
-    message_ex(fd);
-    close(fd);
-    return 0;
-}
-
-
-int conn_serv(void)
+int connect_to_server(void) 
 {
-    int c_fd = (AF_INET, SOCK_STREAM, 0); 
-    struct sockaddr_in srv;
-
-    if(c_fd < 0)
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) 
     {
-        perror("socket"); 
-        exit(1); 
+        perror("socket");
+        exit(1);
     }
 
+    struct sockaddr_in srv;
     memset(&srv, 0, sizeof srv);
     srv.sin_family = AF_INET;
     srv.sin_port = htons(PORT);
-
     if (inet_pton(AF_INET, "127.0.0.1", &srv.sin_addr) <= 0) 
     {
-        perror("inet_pton"); 
-        close(c_fd); 
-        exit (1);
+        perror("inet_pton");
+        close(fd);
+        exit(1);
     }
 
-    if (connect(c_fd, (struct sockaddr*)&srv, sizeof srv) < 0) 
+    if (connect(fd, (struct sockaddr *)&srv, sizeof srv) < 0) 
     {
-        perror("connect"); 
-        close(c_fd); 
-        return 1;
+        perror("connect");
+        close(fd);
+        exit(1);
     }
-    return c_fd; 
+
+    return fd;
 }
 
-void message_ex(int m_fd)
+void exchange_message(int fd) 
 {
     char buf[BUF_SIZE];
-    ssize_t n; 
+    ssize_t n;
 
-    printf("Connected. Type a line and press Enter:");
+    printf("Connected. Type a line and press Enter: ");
     fflush(stdout);
-    if (fgets(buf, sizeof buf, stdin) == NULL) 
-    { 
-        fprintf(stderr, "No input has been received.\n"); 
-        return; 
+
+    if (fgets(buf, sizeof buf, stdin) == NULL) {
+        if (feof(stdin)) {
+            fprintf(stderr, "No input has been received.\n");
+        } else {
+            perror("fgets");
+        }
+        return;
     }
 
-    send(m_fd, buf, strlen(buf), 0); 
-    
-    n = recv(m_fd, buf, sizeof buf - 1, 0);
-    if(n > 0)
-    {
-        buf[n] = '\0';
-        printf("Echo from server: %s", buf);
+    size_t len = strlen(buf);
+    if (len == 0) {
+        fprintf(stderr, "Empty input.\n");
+        return;
     }
-    else
-    {
-        printf("No response or connection closed.\n");
+
+    if (send(fd, buf, len, 0) != (ssize_t)len) {
+        perror("send");
+        return;
     }
+
+    n = recv(fd, buf, sizeof buf - 1, 0);
+    if (n < 0) {
+        perror("recv");
+        return;
+    } else if (n == 0) {
+        printf("Server closed the connection.\n");
+        return;
+    }
+
+    buf[n] = '\0';
+    printf("Echo from server: %s", buf);
+}
+
+int main(void) {
+    int fd = connect_to_server();
+    exchange_message(fd);
+    close(fd);
+    return 0;
 }
